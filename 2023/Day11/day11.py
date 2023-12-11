5259e556750e05ac46561ee85cce357b030075aa
HMFlol <holy.mnemonic@gmail.com>
2023-12-11 14:43:02 -0400

    day11clean

--- 2023/Day11/day11.py
+++ 2023/Day11/day11.py
@@ -4,20 +4,24 @@ The rest is essentially the same
 """
 from aocd import data
 from itertools import combinations
+from time import time
 
+starttime = time()
+
+# Gridding
 grid = [list(line) for line in data.split('\n')]
 
-# Get the rows of all .
-alldotsr = []
+# Get the rows of all dots
+all_dots_r = []
 for r, row in enumerate(grid):
     if all(ch == "." for ch in row):
-        alldotsr.append(r)
+        all_dots_r.append(r)
 
-# Get the columns of all .
-alldotsc = []
+# Get the columns of all dots
+all_dots_c = []
 for c, col in enumerate(zip(*grid)):
     if all(ch == "." for ch in col):
-        alldotsc.append(c)
+        all_dots_c.append(c)
 
 def calculate_sum_of_distances(expansion):
     # Get the indices of the empty rows and columns
@@ -30,8 +34,8 @@ def calculate_sum_of_distances(expansion):
         for c in range(len(grid[0])):
             if grid[r][c] == '#':
                 # Adjust the coordinates to account for the inserted rows and columns
-                r_adjusted = get_index_after_inserts(r, alldotsr)
-                c_adjusted = get_index_after_inserts(c, alldotsc)
+                r_adjusted = get_index_after_inserts(r, all_dots_r)
+                c_adjusted = get_index_after_inserts(c, all_dots_c)
                 hashes.append((r_adjusted, c_adjusted))
 
     # Function to calculate Manhattan distance
@@ -58,8 +62,17 @@ def calculate_sum_of_distances(expansion):
 expansionp1 = 2
 expansionp2 = 1000000
 
-print(calculate_sum_of_distances(expansionp1))
-print(calculate_sum_of_distances(expansionp2))
+start = time()
+print(f"Part 1: {calculate_sum_of_distances(expansionp1)}")
+end = time()
+print(f"Finished in {end - start:.6f} seconds")
 
+start = time()
+print(f"Part 2: {calculate_sum_of_distances(expansionp2)}")
+end = time()
+print(f"Finished in {end - start:.6f} seconds")
+
+endtime = time()
+print(f"Total execution time: {endtime - starttime:.6f} seconds")
 
 
