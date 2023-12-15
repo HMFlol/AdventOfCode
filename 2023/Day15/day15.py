e3cdce9d9ec0010cde0b153b31e1c3000427fef6
HMFlol <holy.mnemonic@gmail.com>
2023-12-15 12:36:28 -0400

    day15

--- 2023/Day15/day15.py
+++ 2023/Day15/day15.py
@@ -5,8 +5,6 @@ data = get_data(day=15, year=2023)
 #data = open('test.txt').read()
 strings = data.split(',')
 
-result = 0
-
 # Lens calc function since I was using the same code in 3 spots
 def lensCalc(label):
     lens = 0
@@ -14,7 +12,10 @@ def lensCalc(label):
         lens = (lens + ord(c)) * 17 % 256
     return lens
 
+
 # Part 1
+result = 0
+
 for string in strings:
     lens = lensCalc(string)
     result += lens
@@ -27,9 +28,9 @@ for string in strings:
     if "-" in string:
         label = string[:-1]
         lens = lensCalc(label)
-        box = boxes[lens] # Get the box at index lens
-        box = [lens for lens in box if lens[0] != label] # Make a new list, including only the lenses that DON'T have the label that we can then pop back in, effectively removing the lens with the given label
-        boxes[lens] = box # Put the box back in the boxes list at the lens index
+        boxminus = boxes[lens] # Get the box at index lens
+        boxminus = [lens for lens in boxminus if lens[0] != label] # Make a new 'box' of all lenses that DON'T have the label that we want to remove
+        boxes[lens] = boxminus # Put the new 'box' back in the boxes list at the lens index, sans label
     else:
         label, focal = string[:-2], string[-1]
         lens = lens = lensCalc(label)
@@ -41,11 +42,14 @@ for string in strings:
             box.append((label, focal)) # If it doesn't exist, just append it
         boxes[lens] = box # Put the box back in the boxes list at the lens index
 
+fpower = sum((b + 1) * (l + 1) * int(focal) for b, box in enumerate(boxes) for l, (_, focal) in enumerate(box))
+'''
+Wrote it like this first time, but I'm practicing my list comprehension skills!
 fpower = 0
 for b, box in enumerate(boxes):
-    for l, lens in enumerate(box, start=1):
-        label, focal = lens
-        fpower += (b + 1) * l * int(focal)
+    for l, (_, focal) in enumerate(box):
+        fpower += (b + 1) * (l + 1) * int(focal)
+'''
 
 print(f"Total (Part1):", result)
 print(f"Total (Part2):", fpower)
