1d5b5a5d2f355b4f9e49b2161412f04f5d227cf6
HMFlol <holy.mnemonic@gmail.com>
2023-12-15 12:42:24 -0400

    day15cleaned

--- 2023/Day15/day15.py
+++ 2023/Day15/day15.py
@@ -12,47 +12,51 @@ def lensCalc(label):
         lens = (lens + ord(c)) * 17 % 256
     return lens
 
-
 # Part 1
-result = 0
+def hash(strings):
+    result = 0
 
-for string in strings:
-    lens = lensCalc(string)
-    result += lens
+    for string in strings:
+        lens = lensCalc(string)
+        result += lens
+
+    return result
 
 # Part 2
+def fpower(strings):
 # Making a bunch of boxes
-boxes = [[] for _ in range(256)]
+    boxes = [[] for _ in range(256)]
 
-for string in strings:
-    if "-" in string:
-        label = string[:-1]
-        lens = lensCalc(label)
-        boxminus = boxes[lens] # Get the box at index lens
-        boxminus = [lens for lens in boxminus if lens[0] != label] # Make a new 'box' of all lenses that DON'T have the label that we want to remove
-        boxes[lens] = boxminus # Put the new 'box' back in the boxes list at the lens index, sans label
-    else:
-        label, focal = string[:-2], string[-1]
-        lens = lens = lensCalc(label)
-        box = boxes[lens] # Get the box at index lens
-        exists = [i for i, lens in enumerate(box) if lens[0] == label] # Get the index of the lens with the given label, if it exists
-        if exists: # If exists is not empty
-            box[exists[0]] = (label, focal) # Replace the 'box' at the given index with the new label
+    for string in strings:
+        if "-" in string:
+            label = string[:-1]
+            lens = lensCalc(label)
+            boxminus = boxes[lens] # Get the box at index lens
+            boxminus = [lens for lens in boxminus if lens[0] != label] # Make a new 'box' of all lenses that DON'T have the label that we want to remove
+            boxes[lens] = boxminus # Put the new 'box' back in the boxes list at the lens index, sans label
         else:
-            box.append((label, focal)) # If it doesn't exist, just append it
-        boxes[lens] = box # Put the box back in the boxes list at the lens index
+            label, focal = string[:-2], string[-1]
+            lens = lens = lensCalc(label)
+            box = boxes[lens] # Get the box at index lens
+            exists = [i for i, lens in enumerate(box) if lens[0] == label] # Get the index of the lens with the given label, if it exists
+            if exists: # If exists is not empty
+                box[exists[0]] = (label, focal) # Replace the 'box' at the given index with the new label
+            else:
+                box.append((label, focal)) # If it doesn't exist, just append it
+            boxes[lens] = box # Put the box back in the boxes list at the lens index
 
-fpower = sum((b + 1) * (l + 1) * int(focal) for b, box in enumerate(boxes) for l, (_, focal) in enumerate(box))
-'''
-Wrote it like this first time, but I'm practicing my list comprehension skills!
-fpower = 0
-for b, box in enumerate(boxes):
-    for l, (_, focal) in enumerate(box):
-        fpower += (b + 1) * (l + 1) * int(focal)
-'''
+    fpower = sum((b + 1) * (l + 1) * int(focal) for b, box in enumerate(boxes) for l, (_, focal) in enumerate(box))
+    '''
+    Wrote it like this first time, but I'm practicing my list comprehension skills!
+    fpower = 0
+    for b, box in enumerate(boxes):
+        for l, (_, focal) in enumerate(box):
+            fpower += (b + 1) * (l + 1) * int(focal)
+    '''
+    return fpower
 
-print(f"Total (Part1):", result)
-print(f"Total (Part2):", fpower)
+print(f"Total (Part1):", hash(strings))
+print(f"Total (Part2):", fpower(strings))
 
 end_time = time()
 print(f"Total execution time: {end_time - start_time:.6f} seconds")
