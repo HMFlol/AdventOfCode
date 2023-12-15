3d9b637f281caa44eda575ab200b745cf5509d90
HMFlol <holy.mnemonic@gmail.com>
2023-12-15 16:41:46 -0400

    day15wowowow

--- 2023/Day15/day15.py
+++ 2023/Day15/day15.py
@@ -25,6 +25,44 @@ def hash(strings):
 # Part 2
 def fpower(strings):
 # Making a bunch of boxes
+    boxes = [dict() for _ in range(256)] # Replaced array of arrays with array of dicts
+    # Using match here is slick. I didn't know about it until I saw it in the solutions.
+    '''
+    for string in strings:
+        match string.strip('-').split('='):
+            case [l, f]: boxes[lensCalc(l)][l] = int(f)
+            case [l]:    boxes[lensCalc(l)].pop(l, 0)
+    '''
+    for string in strings:
+        if "-" in string:
+            label = string[:-1]
+            lens = lensCalc(label)
+            boxes[lens].pop(label, 0) # remove label from box at index lens
+        else:
+            label, focal = string[:-2], string[-1]
+            lens = lens = lensCalc(label)
+            boxes[lens][label] = int(focal) # add label to box at index lens
+
+    fpower = sum(box_idx * l_slot * focal for box_idx, box in enumerate(boxes, 1) for l_slot, focal in enumerate(box.values(), 1))
+
+    return fpower
+
+start_time = time()
+
+print(f"Total (Part1):", hash(strings))
+print(f"Total (Part2):", fpower(strings))
+
+end_time = time()
+print(f"Total execution time: {end_time - start_time:.6f} seconds")
+
+
+'''
+Initial fpower function used an array of arrays which required extra steps.
+Using an array of dicts, as above, allows for a much simpler solution.
+The above function is doing the same thing as below but with much fewer steps.
+
+def fpower(strings):
+# Making a bunch of boxes
     boxes = [[] for _ in range(256)]
 
     for string in strings:
@@ -46,19 +84,4 @@ def fpower(strings):
             boxes[lens] = box # Put the box back in the boxes list at the lens index
 
     fpower = sum((box_idx + 1) * (l_slot + 1) * int(focal) for box_idx, box in enumerate(boxes) for l_slot, (_, focal) in enumerate(box))
-    '''
-    Wrote it like this first time, but I'm practicing my list comprehension skills!
-    fpower = 0
-    for box_idx, box in enumerate(boxes):
-        for l_slot, (_, focal) in enumerate(box):
-            fpower += (box_idx + 1) * (l_slot + 1) * int(focal)
-    '''
-    return fpower
-
-start_time = time()
-
-print(f"Total (Part1):", hash(strings))
-print(f"Total (Part2):", fpower(strings))
-
-end_time = time()
-print(f"Total execution time: {end_time - start_time:.6f} seconds")
+'''
