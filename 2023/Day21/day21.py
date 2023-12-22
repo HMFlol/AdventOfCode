a4fde7f1bd33d7c91ae29afb9c66329a235dcb7b
HMFlol <holy.mnemonic@gmail.com>
2023-12-21 23:23:49 -0400

    day21

--- 2023/Day21/day21.py
+++ 2023/Day21/day21.py
@@ -8,34 +8,32 @@ data = data.strip().splitlines()
 
 grid = {complex(x,y): val for y, row in enumerate(data) for x, val in enumerate(row)}
 
-S = next(x for x in grid if grid[x] == 'S') # Starting pos
+start = next(x for x in grid if grid[x] == 'S') # Starting pos
 
 dirs = [1, -1, 1j, -1j]  # north, south, east, west
 
-def iAmBfs(grid, end):
-    q = deque([(S, 0)])  # Initialize queue with starting position and steps
-    seen = {S}
-    ans = set()
+size = len(data[0]) # Size of the grid
+steps = 26501365
 
-    while q:
-        pos, steps = q.popleft()
-        if steps % 2 == 0:
-            ans.add(pos) # Adding only the even steps to get rid of back and forth movement
-        if steps == end:
-            continue
-        for dir in dirs:
-            new_pos = pos + dir
-            if new_pos in grid and grid[new_pos] in '.S' and new_pos not in seen:
-                seen.add(new_pos)
-                q.append((new_pos, steps + 1))
+def iAmBfs(start, end):
+    pos = {start} # Start at start (S)
 
-    return len(ans)
+    for _ in range(end): # Loop through the number of steps
+        new_pos = set() # Set of new positions possible to move to
+        for p in pos: # Loop through the current positions
+            for dir in dirs: # Loop through the directions
+                if p + dir in grid and grid[p + dir] in '.S': # If the new position is in the grid and is a garden plot or S
+                    new_pos.add(p + dir) # Add the new position to the set of new positions
+        pos = new_pos # Set the current positions to the new positions
 
+    return len(pos) # Return the number of positionsstart
+
+#def idkhonestly(grid, BIG_NUM):
 
 start_time = time()
 
-print(f"Total (Part1):", iAmBfs(grid, 64))
-# print(f"Total (Part2):", iAmBfsInfinite(grid, 26501365))
+print(f"Total (Part1):", iAmBfs(start, 64))
+#print(f"Total (Part2):", idkhonestly(grid, 26501365))
 
 end_time = time()
 print(f"Total execution time: {end_time - start_time:.6f} seconds")
