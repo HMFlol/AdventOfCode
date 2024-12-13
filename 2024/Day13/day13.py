5e70bed87c19a9fbfcfa6d7f28d48970d617b61c
HMFlol <holy.mnemonic@gmail.com>
2024-12-13 16:34:21 -0400

    day13 wow wow wow

--- 2024/Day13/day13.py
+++ 2024/Day13/day13.py
@@ -16,9 +16,7 @@ def find_spend(section, add=0):
     a_presses = (px * by - py * bx) / (ax * by - ay * bx)
     b_presses = (ax * py - ay * px) / (ax * by - ay * bx)
 
-    spend = int(a_presses) * 3 + int(b_presses) if a_presses.is_integer() and b_presses.is_integer() else 0
-
-    return spend
+    return int(a_presses) * 3 + int(b_presses) if a_presses.is_integer() and b_presses.is_integer() else 0
 
 
 start_time = time()
