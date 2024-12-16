eccc9ba98be9435fe186a108e673fadf383190c8
HMFlol <holy.mnemonic@gmail.com>
2024-12-16 15:06:12 -0400

    day one six with some notes

--- 2024/Day16/day16.py
+++ 2024/Day16/day16.py
@@ -10,26 +10,31 @@ from aocd import get_data
 def maze_score(grid):
     # Find starting position marked S
     (start,) = [pos for pos in grid if grid[pos] == "S"]
-    # Start facing east
+    # Tie breaker for the heap due to using complex numbers
     tie = 0
     heap = [(0, tie, start, 1, 0, 0, [start])]
+    # Store the minimum score for each position and direction
     min_score = defaultdict(lambda: float("inf"))
     seen = []
     best = float("inf")
 
     while heap:
         score, _, pos, dir, steps, turns, path = heappop(heap)
+        # If the score is higher than the minimum score for this position and direction, skip
+        # Otherwise, update the minimum score
         if score > min_score[pos, dir]:
             continue
         else:
             min_score[pos, dir] = score
-
+        # If we reached the end and the score is less than the best score, update the best score
+        # and add the path to the seen list
         if grid.get(pos) == "E" and score <= best:
             best = score
             seen += path
-
+        # Try moving in all four directions
         for new_dir in [1, -1, 1j, -1j]:
             new_pos = pos + new_dir
+            # If valid, calculate new score and add it all, including the path, to the heap
             if grid.get(new_pos) in {".", "E"}:
                 new_steps = steps + 1
                 new_turns = turns + (dir != new_dir)
